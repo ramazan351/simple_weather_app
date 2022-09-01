749673b7d78352df303a39cc7b73b22e180cb5bd
ramazan351 <ramazan351.ra@gmail.com>
2022-09-01 20:25:19 +0300

    stroke added

--- lib/custom_paints/button_bg.dart
+++ lib/custom_paints/button_bg.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:simple_weather_project/const_values/colors.dart';
 
 class ButtonBackgroundPaint extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint();
-
-    paint.style = PaintingStyle.fill;
-    paint.strokeWidth = 3.0;
-    paint.shader = const LinearGradient(
-            begin: Alignment.topRight, end: Alignment.bottomLeft,
-            // ignore:
-            colors: [Color(0xff262C51), Color(0xff3E3F74)])
-        .createShader(const Rect.fromLTWH(0, 0, 0, 0));
     final segmentWidth = size.width / 24;
     final segmentHeight = size.height / 9;
+    Paint paintFill = Paint();
 
+    paintFill.style = PaintingStyle.fill;
+    paintFill.strokeWidth = 1.0;
+
+    paintFill.shader = const LinearGradient(
+            begin: Alignment.topCenter, end: Alignment.bottomCenter,
+            // ignore:
+            colors: [Color(0xff262C51), Color(0xff3E3F74)])
+        .createShader(Rect.fromLTWH(segmentWidth * 12, segmentHeight * 5,
+            segmentWidth * 24, segmentHeight * 5));
+
+    Paint paintStroke = Paint();
+    paintStroke.style = PaintingStyle.stroke;
+    paintStroke.strokeWidth = 1.0;
+
+    paintStroke.color = whiteColor.withOpacity(0.3);
     Path path = Path();
     path.moveTo(0, segmentHeight * 9);
     path.cubicTo(
@@ -28,7 +36,8 @@ class ButtonBackgroundPaint extends CustomPainter {
     path.lineTo(segmentWidth * 14, 0);
     path.cubicTo(segmentWidth * 20, 0, segmentWidth * 18, segmentHeight * 8,
         segmentWidth * 24, segmentHeight * 9);
-    canvas.drawPath(path, paint);
+    canvas.drawPath(path, paintFill);
+    canvas.drawPath(path, paintStroke);
   }
 
   @override
